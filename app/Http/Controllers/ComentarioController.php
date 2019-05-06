17b7f7e5bc7d426aaa5898481dbf73eb903c78f6
Pchenrique <pchenrique1998@hotmail.com>
2019-05-06 19:37:52 -0300

    Implementação da verificação da autenticação por usuario as paginas

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -11,6 +11,12 @@ class ComentarioController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         //
--- app/Http/Controllers/PostagemController.php
+++ app/Http/Controllers/PostagemController.php
@@ -11,6 +11,12 @@ class PostagemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         //
