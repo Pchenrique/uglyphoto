eb2042074e66b0f1c2d50f1fd9871d42509ee8ca
Pchenrique <pchenrique1998@hotmail.com>
2019-05-08 02:07:06 -0300

    criação da relação um para um

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function perfil(){
+        return $this->hasOne('App\Perfil');
+    }
 }
