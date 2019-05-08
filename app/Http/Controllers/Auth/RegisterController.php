a1b4c5b3ffec9a9af80132fd47c35b7c979070cb
Pchenrique <pchenrique1998@hotmail.com>
2019-05-08 01:04:01 -0300

    Mudança do caminho do registro, agora para a pagina de criação do perfil

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/perfil/create';
 
     /**
      * Create a new controller instance.
