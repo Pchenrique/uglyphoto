9eb83a29b8fd622f7a1f12ded174338ef26ff681
Pchenrique <pchenrique1998@hotmail.com>
2019-05-11 15:50:12 -0300

    Redirecionamento para a pagina de perfil do usuario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Perfil;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = User::find(auth()->user()->id);
+        $perfil = $user->perfil;
+
+        return view('perfil.exibir',['perfil'=>$perfil]);
     }
+
+
 }
