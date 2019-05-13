89b50c0e010ea7130a14e3a567269e7f28c212f6
samuellima99 <lima300799@gmail.com>
2019-05-13 10:01:13 -0300

    Criação da tela de perfil

--- resources/views/perfil/exibir.blade.php
+++ resources/views/perfil/exibir.blade.php
@@ -1,36 +1,144 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Perfil do {{$perfil->nome}}</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <img src="C:\xampp\htdocs\uglyphoto\public\imagem-perfil\perfil-id_6.jpg" width="200" height="200">
-                    {{$perfil->imagem}}
-                    <p><strong>Nome:</strong> {{$perfil->nome}}</p>
-                    <p><strong>Biografia:</strong> {{$perfil->biografia}}</p>
-                    <p><strong>numero:</strong> {{$perfil->numero}}</p>
-                    <p><a href="{{route('perfil.edit', $perfil->id)}}">Editar Perfil</p>
-                    <p>
-                        <form method="post" action="{{route('perfil.destroy',   $perfil->id)}}">
-                            @csrf
-                            @method('DELETE')
-                            <input type="submit" value="excluir perfil">
-                        </form>
-                    </p>
-                
-        
-                </div>
+    <title>Perfil</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/style_perfil.css')}}">
+</head>
+<body>
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <div class="col-lg-6 text-center">
+                    <img src="{{asset('imagem-perfil/perfil-id_6.jpg')}}" alt="perfil" class="rounded-circle" id="foto" width="170px" height="170px">
+                </div> 
+                <strong>UP</strong>
             </div>
+
+            <div class="text-center"><strong>Perfil do(a)</strong> {{$perfil->nome}}</div>
+
+            <ul class="list-unstyled components">
+                <li class="text-center">
+                    <i class="fas fa-user-edit"></i> <strong>Biografia:</strong> {{$perfil->biografia}}
+                </li>
+                <li class="phone">
+                    <i class="fas fa-phone"></i> <strong>Numero:</strong> {{$perfil->numero}}
+                </li>
+                <li>
+                    <a class="text-center" href="{{route('perfil.edit', $perfil->id)}}"><i class="fas fa-edit"></i> Editar Perfil</a>
+                </li>
+                <li>
+                    <form id="formu" method="post" action="{{route('perfil.destroy',   $perfil->id)}}">
+                        @csrf
+                        @method('DELETE')
+                        <a class="text-center"> <i class="fas fa-trash-alt"></i><input type="submit"  id="excluir" value="Excluir perfil"></a>
+                    </form>
+                </li>
+            </ul>
+            <div class="text-center"><Strong>Amigos</Strong></div>
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a class="text-center" href="#">Amigo 01</a>
+                </li>
+                <li>
+                   <a class="text-center" href="#">Amigo 02</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-outline-info">
+                        <i class="fas fa-angle-double-left"></i>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a  href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();" id="sair">Sair <i class="fas fa-sign-out-alt"></i>
+                                </a> 
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="{{asset('imagem-perfil/perfil-id_6.jpg')}}" alt="Card image cap">
+                <div class="list-group list-group-flush">
+                    <li class="list-group-item">Aqui vai aparecer os likes</li>
+                </div>
+                <div class="card-body" >
+                  <a href="#" class="btn btn-outline-default"><i class="fas fa-thumbs-up"></i> Curtir</a>
+                  <a href="#" class="btn btn-outline-default"><i class="fas fa-comment-alt"></i> Comentar</a>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <!---Aqui precisa fazer um foreach para mostrar todos os comentários--->
+                  <li class="list-group-item">Aqui vai aparecer os comentarios</li>
+                </ul>
+                <form>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escreva um comentário"></textarea>
+                    </div>
+                </form>    
+            </div>
+            
+
+
+
+
+
+
+
+
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+   
+</body>
+</html>
+
