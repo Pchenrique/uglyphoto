649dd1753b94185461b8fd79dd8fd48d980968d5
Pchenrique <pchenrique1998@hotmail.com>
2019-05-13 21:05:01 -0300

    criação do botão postar

--- resources/views/perfil/exibir.blade.php
+++ resources/views/perfil/exibir.blade.php
@@ -71,9 +71,8 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
-                    <button type="button" id="sidebarCollapse" class="btn btn-outline-info">
-                        <i class="fas fa-angle-double-left"></i>
-                    </button>
+                    <a class="text-primary" href="{{route('postagem.create')}}">Nova postagem</a>
+
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
