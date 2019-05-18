9109ad23deea8b7ccf52e39a4592e86d5e9be60b
Pchenrique <pchenrique1998@hotmail.com>
2019-05-18 17:00:29 -0300

    criação da relação com perfil e postagem

--- app/Comentario.php
+++ app/Comentario.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    
+    public function perfil()
+    {
+        return $this->belongsTo('App\Perfil');
+    }
+
+    public function postagem()
+    {
+        return $this->belongsTo('App\Postagem');
+    }
 }
