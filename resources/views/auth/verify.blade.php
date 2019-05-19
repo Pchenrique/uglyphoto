ca33163c0cab59dcb1a25b6bf4910e1a349d5248
samuellima99 <lima300799@gmail.com>
2019-05-19 13:25:26 -0300

    Alteração da tela de verificação de senha.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifique seu endereço de e-mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Um novo link de verificação foi enviado para o seu endereço de e-mail.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('Antes de prosseguir, verifique seu e-mail em busca de um link de verificação.') }}
+                    {{ __('Se você não recebeu o email') }}, <a href="{{ route('verification.resend') }}">{{ __('clique aqui para solicitar outro') }}</a>.
                 </div>
             </div>
         </div>
