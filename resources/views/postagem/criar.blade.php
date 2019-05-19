1e4c086237128beaca76c1ab433fdcc6fe7b8866
samuellima99 <lima300799@gmail.com>
2019-05-19 13:28:14 -0300

    Alteração da tela de postagens.

--- resources/views/postagem/criar.blade.php
+++ resources/views/postagem/criar.blade.php
@@ -1,44 +1,118 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Nova Postagem</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/Style_postagem.css')}}">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{asset('img/logo.png')}}" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar-se') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Deslogar') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Nova Postagem</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            @if (session('erro'))
+                                <div class="alert alert-danger">
+                                    {{ session('erro') }}
+                                </div>
+                            @endif
+
+                            <form action="{{route('postagem.store')}}" method="POST" enctype="multipart/form-data">
+                                @csrf
+
+                                    <div class="form-group"> 
+                                        <label>Foto da Postagem</label><br>
+                                        <input class="" name="caminho" type="file">
+                                    </div>
+
+                                    <div class="form-group"> 
+                                        <label>Legenda</label>  
+                                        <input class="form-control" name="legenda" type="text">
+                                    </div>
+
+                                    <button class="btn btn-outline-info" type="submit" id="postagem_btn">Postar</button>   
+                                
+                            </form>
+
                         </div>
-                    @endif
-                    @if (session('erro'))
-                        <div class="alert alert-danger">
-                            {{ session('erro') }}
-                        </div>
-                    @endif
-
-                    <form action="{{route('postagem.store')}}" method="POST" enctype="multipart/form-data">
-						@csrf
-
-							<div class="form-group"> 
-					       		<label>Foto da Postagem</label><br>
-					            <input class="" name="caminho" type="file">
-					        </div>
-
-					       	<div class="form-group"> 
-					       		<label>Legenda</label>  
-					            <input class="form-control" name="legenda" type="text">
-					        </div>
-
-					        <button type="submit">Postar</button>   
-					    
-					</form>
-
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
